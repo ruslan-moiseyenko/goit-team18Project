4cc9b6383ffec6ed4087e74a870392b1b604f661
ruslan-moiseyenko <50419466+ruslan-moiseyenko@users.noreply.github.com>
2021-12-03 16:48:55 +0000

    Deploying to gh-pages from @ ruslan-moiseyenko/goit-team18Project@36eeca34dec7b0dd62fb947f3dd602e75cbf1b2c 🚀

--- src.e5e23e97.js
+++ src.e5e23e97.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-team18Project/src.4b036a67.js.map
+//# sourceMappingURL=/goit-team18Project/src.e5e23e97.js.map
