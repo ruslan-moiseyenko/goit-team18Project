f8a0a37dd7841a5abc5e4edd9177a4744bdd1a44
maria-moroz <m0954008202@gmail.com>
2021-12-07 19:46:11 +0200

    Update

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,6 +3,7 @@
     const closeMenuBtnRef = document.querySelector("[data-close-menu-button]");
     const buyButtonRef = document.querySelector("[data-buy-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const mobileMenuItemsRef = document.querySelectorAll("[data-mobile-nav]");
 
     openMenuBtnRef.addEventListener("click", () => {
         openMenuBtnRef.classList.add("is-open");
@@ -15,5 +16,12 @@
         buyButtonRef.classList.remove("is-open");
         mobileMenuRef.classList.remove("is-open");
     });
-    
+
+    mobileMenuItemsRef.forEach(function (item) {
+        item.addEventListener("click", () => {
+            openMenuBtnRef.classList.remove("is-open");
+            buyButtonRef.classList.remove("is-open");
+            mobileMenuRef.classList.remove("is-open");
+        });
+    });
 })();
